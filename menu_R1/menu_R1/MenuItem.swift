b22edb0250871dda2cf7c3ac5813bdb126400ac7
Mabende <belindamabende@gmail.com>
2024-05-09 11:59:02 +0200

    Take me back to day 1.

---
+++ menu_R1/menu_R1/MenuItem.swift
@@ -0,0 +1,16 @@
+//
+//  MenuItem.swift
+//  menu_R1
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/09.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable{
+    
+    var id: UUID = UUID()
+    var imageName:String
+    var name:String
+    var price:String
+}
